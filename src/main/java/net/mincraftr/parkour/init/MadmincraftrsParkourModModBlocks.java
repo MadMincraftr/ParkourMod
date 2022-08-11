9e96303b2caa65523dfbc2a3db011e34ce84999d
madtheanx1 <Thean@DESKTOP-AOI9OST>
2022-08-11 11:35:54 -0500

    Project upload

---
+++ src/main/java/net/mincraftr/parkour/init/MadmincraftrsParkourModModBlocks.java
@@ -0,0 +1,19 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mincraftr.parkour.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mincraftr.parkour.block.KillBrickBlock;
+import net.mincraftr.parkour.MadmincraftrsParkourModMod;
+
+public class MadmincraftrsParkourModModBlocks {
+	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, MadmincraftrsParkourModMod.MODID);
+	public static final RegistryObject<Block> KILL_BRICK = REGISTRY.register("kill_brick", () -> new KillBrickBlock());
+}
